2dd94eddb3b9eec0e2767b2cf9268e04a612d971
OnsHanafi <55098085+OnsHanafi@users.noreply.github.com>
2025-01-12 09:45:24 +0100

    added sp password resource

--- Azure_Infrastructure_Terraform/modules/servicePrincipal/main.tf
+++ Azure_Infrastructure_Terraform/modules/servicePrincipal/main.tf
@@ -10,3 +10,7 @@ resource "azuread_service_principal" "example" {
   app_role_assignment_required = true
   owners                       = [data.azuread_client_config.current.object_id]
 }
+
+resource "azuread_service_principal_password" "example" {
+  service_principal_id = azuread_service_principal.example.id
+}
